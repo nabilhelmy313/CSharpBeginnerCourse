6e50689913c84fe58afc544841e3382178330600
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2023-09-16 15:25:24 +0300

    inputandoutput

---
+++ inputoutputandcomment/inputoutputandcomment/Program.cs
@@ -0,0 +1,7 @@
+﻿//this is output 
+Console.WriteLine("please put Your Name");
+/*this the input will 
+ we work on*/
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}");
+Console.ReadKey();
