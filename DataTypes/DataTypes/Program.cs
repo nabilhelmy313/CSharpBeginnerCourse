295a1eae111eb9d074738ecbdfd7d08e42174a3d
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2023-09-16 16:03:47 +0300

    DataTypes

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//Value Types
+int myInteger = 42;
+Console.WriteLine($"int {myInteger}");
+double myDouble = 3.14159;
+Console.WriteLine($"double {myDouble}");
+bool isTrue = true;
+Console.WriteLine($"boolean {isTrue}");
+
+//Referance Types
+string myString = "Hello, World!";
+Console.WriteLine($"string {myString}");
+
+Console.ReadKey();
