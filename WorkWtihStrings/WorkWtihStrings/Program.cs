70bf41bb4b5575116d9e386afbf7d2fad60e37f0
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2023-09-27 03:16:00 +0300

    Work With String

---
+++ WorkWtihStrings/WorkWtihStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿string hello = "Hello, World!";
+Console.WriteLine(hello);
+
+string firstName = "Nabil";
+string lastName = "Helmy";
+string fullName = firstName + " " + lastName; // Concatenation
+Console.WriteLine(fullName);
+
+string helloNabil = "Hello, Nabil!";
+string nabil = helloNabil.Substring(7, 5); // sub is "Hello"
+Console.WriteLine(nabil);
+
+string name = "Ahmed";
+int age = 30;
+string message = $"My name is {name} and I am {age} years old.";//string Interpolation
+Console.WriteLine(message);
+Console.WriteLine(message + message.Length);
