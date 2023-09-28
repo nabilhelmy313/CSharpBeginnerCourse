fa8b19f58c5eedbcbca4f644a7acf4d1490bc5a9
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2023-09-28 11:28:01 +0300

    ifCondtionWithGlobalUsing

---
+++ IfCondion/IfCondion/Program.cs
@@ -0,0 +1,49 @@
+﻿global using static System.Console;//.net 7 feature
+int number = 10;
+
+if (number > 0)
+{
+    WriteLine("The number is positive.");
+}
+else if (number == 0)
+{
+    WriteLine("The number is zero.");
+}
+else
+{
+    WriteLine("The number is negative.");
+}
+
+// Nested if statements
+int age = 25;
+bool isStudent = true;
+
+if (age >= 18)
+{
+    WriteLine("You are an adult.");
+
+    if (isStudent)
+    {
+        WriteLine("You are a student.");
+    }
+    else
+    {
+        WriteLine("You are not a student.");
+    }
+}
+else
+{
+    Console.WriteLine("You are a minor.");
+}
+
+// Ternary conditional operator
+int x = 5;
+int y = 10;
+string result = (x > y) ? "x is greater" : "y is greater";
+WriteLine(result);
+
+
+
+
+
+
