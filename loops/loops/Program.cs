b520aecc26471ef550b99ae334c7dd75a9892648
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2023-10-01 02:15:05 +0300

    loops

---
+++ loops/loops/Program.cs
@@ -0,0 +1,30 @@
+﻿global using static System.Console;
+
+Write("Enter count of printing : ");
+int forCount = int.Parse(ReadLine());
+
+// Using a for loop
+for (int i = 0; i < forCount; i++)
+{
+    WriteLine($"hi from For loop {i + 1}");
+}
+
+// Using a while loop
+Write("Enter count of printing : ");
+int whileCount = int.Parse(ReadLine());
+
+int j = 0;
+while (j < whileCount)
+{
+    WriteLine($"Welcome form while {j}");
+    j++;
+}
+// Using a do-while loop
+Write("Enter count of printing : ");
+int doWhileCount = int.Parse(ReadLine());
+int k = 0;
+do
+{
+    WriteLine($"Welcome From DO While {k + 1}");
+    k++;
+} while (k < doWhileCount);
