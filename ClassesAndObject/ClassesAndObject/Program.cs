190d00ed641d575b5708d3194fbf94b16409a8d4
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2023-10-06 11:26:52 +0300

    ClassesAndObject

---
+++ ClassesAndObject/ClassesAndObject/Program.cs
@@ -0,0 +1,15 @@
+﻿global using static System.Console;
+using ClassesAndObject;
+Product pen = new Product();
+pen.Name = "Pen 1";
+pen.Price = 10.5;
+
+Product notebook = new Product();
+notebook.Name = "Note Book";
+notebook.Price = 15;
+WriteLine($"Pen Name is {pen.Name} & {pen.Price}  EGP ");
+WriteLine($"The Notebook Name is" +
+    $" {notebook.Name} & {notebook.Price}  EGP ");
+
+
+
