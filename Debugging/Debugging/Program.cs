48cb776f29b2cbab786954d053792224237373dc
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2023-10-13 14:03:47 +0300

    debugging

---
+++ Debugging/Debugging/Program.cs
@@ -0,0 +1,15 @@
+﻿char[] letters = 
+    { 'N', 'a', 'b', 'i', 'l',' ', 
+    'H', 'e', 'l', 'm', 'y' };
+string name = "";
+int[] a = new int[11];
+for (int i = 0; i < letters.Length; i++)
+{
+    name += letters[i];
+    a[i] = i + 1;
+    SendMessage(name, a[i]);
+}
+static void SendMessage(string name, int msg)
+{
+    Console.WriteLine("Hello, " + name + "! Count to " + msg);
+}
