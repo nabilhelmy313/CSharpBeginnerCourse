4a65028fc49197e1a031e6b5afaf48fc2ad2302f
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2023-10-23 15:49:20 +0300

    Properties

---
+++ Properties/Properties/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace Properties
+{
+    internal class Product
+    {
+        // the name field
+        private string _name;
+        private double _price;
+        private string _description;
+        // the Name property For Get
+        public string PropertyName { get => _name; set => _name = value; }
+        public int Quantity { get; set; }
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value * value * 0.14; }
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+        public void SetDescription(string description)
+        {
+            _description = description;
+        }
+
+    }
+}
---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,16 @@
+﻿global using static System.Console;
+using Properties;
+
+Product product = new Product();
+
+// Set values for the properties
+product.PropertyName = "Example Product";
+product.Quantity = 10;
+product.Price = 100.0;
+product.SetDescription("This is an example product.");
+
+// Display the property values and description
+WriteLine("Product Name: " + product.PropertyName);
+WriteLine("Quantity: " + product.Quantity);
+WriteLine("Price: " + product.Price);
+WriteLine("Description: " + product.GetDescription()); // the get accessor is invoked here
